864929c35278537125cba22eb3f0fc28ff1f5b80
Akshatha Savanth15 <asavan15@in.ibm.com>
2021-09-05 19:08:36 +0530

    initial commit

---
+++ src/test/java/webdriverMavenGitSample/LoginTest.java
@@ -0,0 +1,41 @@
+package webdriverMavenGitSample;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	String baseURL="https://courses.letskodeit.com/login";
+	
+	@BeforeTest
+	public void setup()
+	{
+		driver=new ChromeDriver();
+		
+	}
+	@Test
+	public void Login()
+	{
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(baseURL);
+		driver.findElement(By.id("email")).sendKeys("akshata@gmail.com");
+		driver.findElement(By.name("password")).sendKeys("Aksh");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
